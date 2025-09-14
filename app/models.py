6d939cb80f85483af0bbc8db3c830a87a7fe6115
Shannon Rogers <rogers.shannon22@gmail.com>
2025-09-13 18:20:51 -0700

    password update

--- app/models.py
+++ app/models.py
@@ -57,7 +57,7 @@ class Mechanics(Base):
     first_name: Mapped[str] = mapped_column(String(120), nullable=False)
     last_name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(360), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    password: Mapped[str] = mapped_column(String(500), nullable=False)
     salary: Mapped[float] = mapped_column(Float, nullable=False)
     address: Mapped[str] = mapped_column(String(500), nullable=False)
     
